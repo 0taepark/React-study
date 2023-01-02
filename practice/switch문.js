e58ab0101c2d9c8362ad0b1c9672f54e282e5a2b
0taepark <park-youngtae@bag-yeongtaeui-MacBookPro.local>
2023-01-02 21:13:10 +0900

    switch문

---
+++ practice/switch문.js
@@ -0,0 +1,19 @@
+let country = "독일";
+
+switch (country) {
+  case "ko":
+    console.log("한국");
+    break;
+  case "cn":
+    console.log("중국");
+    break;
+  case "jp":
+    console.log("일본");
+    break;
+  case "uk":
+    console.log("영국");
+    break;
+  default:
+    console.log("미 분류");
+    break;
+}
