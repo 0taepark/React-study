f10e8688b0b9b7007397abde9fb2d26056b28506
0taepark <ytpark1665@gmail.com>
2023-01-15 18:15:08 +0900

    리액트 배열

---
+++ practice/배열.js
@@ -0,0 +1,21 @@
+let arr = new Array();
+let arr1 = []; //배열 리터럴
+console.log(arr)
+console.log(arr1)
+
+let arr3 = [1, "2", true, null, undefined, {}, []]
+console.log(arr3)
+
+let arr4 = [1, 2, 3, 4, 5]
+console.log(arr4)
+console.log(arr4[0])
+
+arr4.push(6);
+console.log(arr4)
+
+arr4.push({key: "value"})
+console.log(arr4)
+
+//배열의 길이
+let arr5 = [1, 2, 3, 4, 5]
+console.log(arr5.length)
